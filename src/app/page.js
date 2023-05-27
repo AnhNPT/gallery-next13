c0d28aee79ed37fe347156d1387c39dfba91b518
Emily <anhnpt2000@gmail.com>
2023-05-28 00:47:24 +0700

    restyle grid layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,34 +4,36 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="bg-[#121214] h-full w-full">
-            <div className="container flex mx-auto pt-36">
+            <div className="container flex mx-auto pt-36 pb-8">
                 {/* Heading */}
                 <span className="text-white mr-44 font-normal text-5xl block leading-[60px]">
                     The — <br /> Abstract <br /> Gallery
                 </span>
                 {/* Img */}
-                <div className="flex flex-wrap gap-8">
-                    {/* Row */}
-                    <div class="grid-container gap-8 w-full">
-                        <div class="column-1">
-                            <img src="/image/img-here.jpg" alt="Image 1" className="h-full"></img>
-                        </div>
-                        <div class="column-2 grid gap-8">
-                            <img src="/image/img-here-1.jpg" alt="Image 2"></img>
-                            <img src="/image/img-here-2.jpg" alt="Image 3"></img>
-                        </div>
+                <div className="grid-container gap-8 w-full">
+                    {/* Row 1 */}
+                    <div className="layoutItem">
+                        <img src="/image/img-here.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
                     </div>
-                    {/* Row */}
-                    <img src="/image/img-here.jpg" alt="Image 2" className="w-full h-[400px] object-cover"></img>
-                    {/* Row */}
-                    <div class="grid-container gap-8 w-full">
-                        <div class="column-1">
-                            <img src="/image/img-here-3.jpg" alt="Image 1" className="h-full"></img>
-                        </div>
-                        <div class="column-2 grid gap-8">
-                            <img src="/image/img-here-4.jpg" alt="Image 2"></img>
-                            <img src="/image/img-here-5.jpg" alt="Image 3"></img>
-                        </div>
+                    <div className="layoutItem">
+                        <img src="/image/img-here-2.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
+                    </div>
+                    <div className="layoutItem">
+                        <img src="/image/img-here-3.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
+                    </div>
+                    {/* Row 2 */}
+                    <div className="layoutItem fullWidth">
+                        <img src="/image/img-here-4.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
+                    </div>
+                    {/* Row 3 */}
+                    <div className="layoutItem">
+                        <img src="/image/img-here-5.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
+                    </div>
+                    <div className="layoutItem layout2">
+                        <img src="/image/img-here-6.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
+                    </div>
+                    <div className="layoutItem">
+                        <img src="/image/img-here-7.jpg" alt="Image 2" className="w-full h-full object-cover"></img>
                     </div>
                 </div>
             </div>
